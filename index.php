62e1c148e6e86298359d52f900e37fb9971f984f
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-10-02 16:53:01 +0200

    inserted with bootstrap input groups for paragraph and word to censor

--- index.php
+++ index.php
@@ -10,13 +10,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Bad Words</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
 </head>
 <body>
-<h1>
-    <?php echo $projectTitle ?>
-</h1>
+<form class="container w-75">
+<h1 class="text-primary"> <?php echo $projectTitle    ?>   </h1>
 
+<div class="form-group mt-5">
+    <label for="badwords-paragraph">Write a paragraph!</label>
+    <textarea class="form-control" id="badwords-paragraph" rows="3"></textarea>
+  </div>
+
+
+
+  <div class="form-group">
+    <label for="badwords-censored-word">Write a word to Censor!</label>
+    <input type="text" class="form-control" id="badwords-censored-word" placeholder="">
+  </div>
+</form>
 
 </body>
 </html>
