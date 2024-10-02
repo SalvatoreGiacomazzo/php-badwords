4a623f713b30968848124d97183874ccc70b06b8
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-10-02 16:16:00 +0200

    var dump tryout

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php     
-   echo 'PHP Bad Words'
+   $projectTitle = "PHP Bad Words"; 
+
+  
+
+   var_dump($projectTitle);
 
     ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>PHP Bad Words</title>
-    </head>
-    <body>
-       
-    </body>
-    </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Bad Words</title>
+    
+</head>
+<body>
+
+</body>
+</html>
