103da08b6606a4744d9c025eff4bd45f967b85be
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-10-02 17:15:02 +0200

    communication tryout between the 2 files

--- badwords.php
+++ badwords.php
@@ -1,3 +1,12 @@
+<?php
+$paragraph = $_GET["badwordsParagraph"];
+
+$censor = $_GET["badwordsCensor"];
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +16,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>testing</h1>
+   <h1> <?php echo $paragraph ?></h1>
+   <h1><?php echo $censor ?>   </h1>
 </body>
 </html>
