f9444c8a43b6611b8c8d6de47a5cfcb61c38c976
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-10-02 15:53:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php     
+
+
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Bad Words</title>
+    </head>
+    <body>
+        <h1>PHP BAD WORDS</h1>
+    </body>
+    </html>
